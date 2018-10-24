8d88014e3ab2c9dc1017c58f17187574672913f5
Jose Llorca <josellorcaflo@gmail.com>
2018-10-24 19:13:28 +0000

    Miercoles 24 Octubre

---
+++ public/DW/Preparado Para el Examen/01.js
@@ -0,0 +1,25 @@
+
+/*1. Realiza un programa que solicite el nombre de un país de la Unión Europea,
+y que indique si pertenece a la zona euro. En caso de ser un país de la unión europea y no pertenecer al euro debe indicar su moneda.
+Si no es un país de la Unión debe volver a pedir el nombre de un pais.(2,5 ptos)
+
+Los 19 estados que forman la zona del euro son:​ Alemania, Austria, Bélgica, Chipre, Eslovaquia, Eslovenia,
+España, Estonia, Finlandia, Francia, Grecia, Irlanda, Italia, Letonia, Lituania, Luxemburgo, Malta, Países Bajos y Portugal.
+
+
+Rumania: 2022.6​
+Croacia: En torno a 2025.7​
+Bulgaria: No se plantea adoptar el euro de momento, aunque debe hacerlo. Está planteando incoporarse al MTC II.8​9​
+Hungría: No se plantea adoptar el euro de momento, aunque debe hacerlo.
+Polonia: No se plantea adoptar el euro de momento, aunque debe hacerlo.
+República Checa: No se plantea adoptar el euro de momento, aunque debe hacerlo.
+Suecia: No se plantea adoptar el euro de momento, aunque debe hacerlo.
+Dinamarca: Tiene una cláusula de exclusión.
+Reino Unido: Tiene una cláusula de exclusión.*/
+
+
+var pais = ['Alemania', 'Austria', 'Bélgica', 'Chipre', 'Eslovaquia', 'Eslovenia',
+'España', 'Estonia', 'Finlandia', 'Francia', 'Grecia', 'Irlanda', 'Italia', 'Letonia', 'Lituania', 'Luxemburgo', 'Malta', 'Países Bajos','Portugal'];
+
+
+alert(pais);
