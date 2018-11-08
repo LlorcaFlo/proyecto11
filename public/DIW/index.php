d9eca77b21d26135eb66a6429fff67961f4b497c
Jose Llorca <josellorcaflo@gmail.com>
2018-11-08 15:25:17 +0000

    Jueves 8 Noviembre

--- public/DIW/index.php
+++ public/DIW/index.php
@@ -1,11 +1,16 @@
+<link rel="stylesheet" type="text/css" href="index.css">
 
-<h1>Css</h1>
 
 <ol>
+<div class="uno">
+	<h1>Css</h1>
+	<div class="dos">
 	<li><a href="4/4.html">Ejercicio 4</a></li>
-  <li><a href="5/5.html">Ejercicio 5</a></li>
-  <li><a href="6/6.html">Ejercicio 6</a></li>
-  <li><a href="7/7.html">Ejercicio 7</a></li>
-  <li><a href="8/8.html">Ejercicio 8</a></li>
-
+  	<li><a href="5/5.html">Ejercicio 5</a></li>
+  	<li><a href="6/6.html">Ejercicio 6</a></li>
+  	<li><a href="7/7.html">Ejercicio 7</a></li>
+  	<li><a href="8/8.html">Ejercicio 8</a></li>
+  	<li><a href="9/9.html">Ejercicio 9</a></li>
+</div>
+</div>
 </ol>
